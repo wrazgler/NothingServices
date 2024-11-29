736f78fc92cabf573d7beb1f81edb7219dd5ecfd
Nikita Golovin <wrazgler@mail.ru>
2024-11-29 15:00:47 +0500

    Перемести тестл

--- NothingServices.WPFApp.UnitTests/ViewModelsTests/NothingModelsListVMTests.cs
+++ NothingServices.WPFApp.UnitTests/ViewModelsTests/NothingModelsListVMTests.cs
@@ -1,8 +1,11 @@
+using System.Collections.ObjectModel;
 using NothingServices.WPFApp.Commands;
 using NothingServices.WPFApp.Models;
 using NothingServices.WPFApp.Services;
+using NothingServices.WPFApp.Strategies;
 using NothingServices.WPFApp.ViewModels;
 using NothingServices.WPFApp.ViewModels.Buttons;
+using NothingServices.WPFApp.ViewModels.Controls;
 
 namespace NothingServices.WPFApp.UnitTests.ViewModelsTests;
 
@@ -42,5 +45,36 @@ public class NothingModelsListVMTests
 
         //Assert
         Assert.False(result);
+    }
+
+    [Fact]
+    public void NothingModels_Equivalent()
+    {
+        //Arrange
+        var mainWindowManager = new MainWindowManager();
+        var nothingModelsListVM = new NothingModelsListVM(
+            mainWindowManager,
+            Mock.Of<IBackButtonVM>(),
+            Mock.Of<IOpenNothingModelsListCommand>());
+        var nothingModels = new ObservableCollection<INothingModelVM>()
+        {
+            Mock.Of<INothingModelVM>(nothingModelVM
+                => nothingModelVM.Id == 1 && nothingModelVM.Name == "Test 1"),
+            Mock.Of<INothingModelVM>(nothingModelVM
+                => nothingModelVM.Id == 2 && nothingModelVM.Name == "Test 2"),
+        };
+        var strategyMock = new Mock<INothingApiClientStrategy>();
+        strategyMock
+            .Setup(strategy => strategy.GetNothingModels(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(nothingModels);
+
+        //Act
+        mainWindowManager.Strategy = strategyMock.Object;
+        mainWindowManager.Next(MainWindowContentType.NothingModelsListVM);
+        var result = nothingModelsListVM.NothingModels;
+
+        //Assert
+        var expected = nothingModels;
+        Assert.Equivalent(expected, result);
     }
 }
